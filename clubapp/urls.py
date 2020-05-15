71c8f553e6f09900332f3e89258f47e829c1b4ca
Titane73 <thomas.ti.hall@gmail.com>
2020-05-15 16:26:55 -0700

    2020May15 Assignment 5

--- clubapp/urls.py
+++ clubapp/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # default index page
     path('', views.index, name='index'),
+    # reference to getResources method in views.py
     path('getResources/', views.getResources, name='resources')
 
 ]

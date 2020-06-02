f63934b17cc91d20743e56c6a31cf2148b38ba91
Titane73 <thomas.ti.hall@gmail.com>
2020-06-02 10:11:19 -0700

    2020June2

--- clubapp/tests.py
+++ clubapp/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Meetings, Minutes, Resources
 
 # Create your tests here.
+class MeetingsTest(TestCase):
+    meeting=Meetings(meetname='townhall')
+    self.assertEqual(str(meeting), meeting.meetname)
+
+def test_table(self):
+    self.assertEqual(str(Meetings._meta.db_talbe), 'Meetings')
